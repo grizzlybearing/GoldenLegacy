bfad9b126c3c9f4dd8cde3568078d2ec69d297c3
Ann <93132135+grizzlybearing@users.noreply.github.com>
2021-12-22 03:17:14 +0300

    last checked

--- src/test/java/GameTetrisTest.java
+++ src/test/java/GameTetrisTest.java
@@ -14,5 +14,8 @@ public class GameTetrisTest {
     public void isComplexCalculations3WorksCorrectly(){
         Assert.assertEquals(125, GameTetris.complexCalculations3(125));
     }
-    
+    @Test
+    public void isComplexCalculations4WorksCorrectly(){
+        Assert.assertEquals(125, GameTetris.complexCalculations4(125));
+    }
 }
